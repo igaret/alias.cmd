79b0d3fb41937d14a90d9fbafababc3729a24676
Friar Izryel <garet.mccallister@gmail.com>
2023-09-15 17:29:56 -0700

    Delete install.bat

--- install.bat
+++
@@ -1,5 +0,0 @@
-curl https://raw.githubusercontent.com/izryel/alias.cmd/master/alias.cmd --output %ProgramData%\alias.cmd 
-mkdir %systemdrive%\Walls
-setx PATH "%PATH%;%SystemDrive%\Walls" /M
-set PATH="%PATH%;%SystemDrive%\Walls"
-copy  %ProgramData%\alias.cmd %SystemDrive%\Walls\ /y
